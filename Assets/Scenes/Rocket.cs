61a0c30c6bfe7312e587d9d8864cc70eeb2ec974
shawnyplayz <55237616+shawnyplayz@users.noreply.github.com>
2019-12-21 08:45:41 +0530

    rotation

--- Assets/Scenes/Rocket.cs
+++ Assets/Scenes/Rocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,16 +23,17 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            print("Space pressed");
+            rigidBody.AddRelativeForce(Vector3.up);
             
         }
         if (Input.GetKey(KeyCode.A))
          {
-            print("Rotating left");
+            transform.Rotate(Vector3.forward);
+            
         }
         else if (Input.GetKey(KeyCode.D))
             {
-            print("Rotating right");
+            transform.Rotate(-Vector3.forward);
         }
         
     }
